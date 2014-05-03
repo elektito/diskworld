bae3956a7dad98b183c8af97df5e1fec875f27a2
Mostafa Razavi <mostafa@sepent.com>
2014-05-03 19:35:58 +0430

    Removed unused module material.py.

--- material.py
+++
@@ -1,8 +0,0 @@
-class Material:
-    def __init__(self, name, color, density):
-        self.name = name
-        self.color = color
-        self.density = density
-
-    def __repr__(self):
-        return "<Material '{}'>".format(self.name)
