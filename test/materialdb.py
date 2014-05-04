43bb2845f9ded25053912a34fb47fffb9da47cbe
Mostafa Razavi <mostafa@sepent.com>
2014-05-04 11:53:34 +0430

    Removed unneeded file.

--- test/materialdb.py
+++
@@ -1,7 +0,0 @@
-from color import white
-from material import Material
-
-class TestMaterialDatabase:
-    def __init__(self):
-        m1 = Material("mat1", white, 0.1)
-        self.testMaterial1 = m1
